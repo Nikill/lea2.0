a64314e2d7023cb9c0a6310d69edbbf17c358d0e
Epo <gomez.gwendo@gmail.com>
2016-11-13 15:24:54 +0100

    Retour des pages manquantes !

---
+++ Lea/src/AppBundle/Form/UserType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('email', EmailType::class)
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('adresse', TextType::class)
+            ->add('ville', TextType::class)
+            ->add('codePostal', TextType::class)
+            ->add('telephoneFix', TextType::class)
+            ->add('telephonePortable', TextType::class)
+            ->add('fax', TextType::class)
+            ->add('dateNaissance', BirthdayType::class)
+            ->add('estHandicape', CheckboxType::class, array("required" => false))
+            ->add('save', SubmitType::class, array('label' => 'Créer un utilisateur'))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+}
